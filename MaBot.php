accb99f27baf54682887152067364d7d452ab524
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-08 22:37:06 +0700

    Code

--- MaBot.php
+++ MaBot.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "Test Test Ma Bot";
 $access_token = 'tYLkTfUwWhH5WVunO5G0QhjUYgiAH4Bd0Bb+oFw0pfis0E0cibf6U73f7gdZid4cUcAURkLMr3D3qW3CfRbuFw3XubbKtHHY14ncqIhRpOwaB5c0BFol/ca78jdM5uCj+bDPDMfEA8bOT/cC0AAV8gdB04t89/1O/w1cDnyilFU='
 $channel_id = '1529179895'
 $channel_secret = '29834a3e45690862a8876a576bac2172'
