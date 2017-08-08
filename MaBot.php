46ca3fc266669b22d35bde708d1061dfd19c3c1d
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-08 22:39:34 +0700

    Fix syntax

--- MaBot.php
+++ MaBot.php
@@ -1,9 +1,9 @@
 <?php
 echo "Test Test Ma Bot";
-$access_token = 'tYLkTfUwWhH5WVunO5G0QhjUYgiAH4Bd0Bb+oFw0pfis0E0cibf6U73f7gdZid4cUcAURkLMr3D3qW3CfRbuFw3XubbKtHHY14ncqIhRpOwaB5c0BFol/ca78jdM5uCj+bDPDMfEA8bOT/cC0AAV8gdB04t89/1O/w1cDnyilFU='
-$channel_id = '1529179895'
-$channel_secret = '29834a3e45690862a8876a576bac2172'
-$userid = 'Ua9222d61b45ff88cc7315440f4f285f3'
+$access_token = 'tYLkTfUwWhH5WVunO5G0QhjUYgiAH4Bd0Bb+oFw0pfis0E0cibf6U73f7gdZid4cUcAURkLMr3D3qW3CfRbuFw3XubbKtHHY14ncqIhRpOwaB5c0BFol/ca78jdM5uCj+bDPDMfEA8bOT/cC0AAV8gdB04t89/1O/w1cDnyilFU=';
+$channel_id = '1529179895';
+$channel_secret = '29834a3e45690862a8876a576bac2172';
+$userid = 'Ua9222d61b45ff88cc7315440f4f285f3';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
