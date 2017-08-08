dece72e9339beaa402bf279803ff5b30a6e8c6a3
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-08 23:22:51 +0700

    Update

--- MaBot.php
+++ MaBot.php
@@ -1,4 +1,9 @@
 <?php
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\Exception\InvalidEventRequestException;
+use LINE\LINEBot\Exception\InvalidSignatureException;
 
 $access_token = 'tYLkTfUwWhH5WVunO5G0QhjUYgiAH4Bd0Bb+oFw0pfis0E0cibf6U73f7gdZid4cUcAURkLMr3D3qW3CfRbuFw3XubbKtHHY14ncqIhRpOwaB5c0BFol/ca78jdM5uCj+bDPDMfEA8bOT/cC0AAV8gdB04t89/1O/w1cDnyilFU=';
 $channel_id = '1529179895';
@@ -7,22 +12,52 @@ $userid = 'Ua9222d61b45ff88cc7315440f4f285f3';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channel_secret]);
 
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
-	$events = $bot->parseEventRequest($req->getBody(), $signature[0]);
-} 
+  $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+} catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+  error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+  error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+  error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
+} catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+  error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
+}
 foreach ($events as $event) {
-	if (!($event instanceof MessageEvent)) {
-	    $logger->info('Non message event has come');
-	    continue;
-	}
-	if (!($event instanceof TextMessage)) {
-	    $logger->info('Non text message has come');
-	    continue;
-	}
-	$replyText = $event->getText();
-	$logger->info('Reply text: ' . $replyText);
-	$resp = $bot->replyText($event->getReplyToken(), $replyText);
-	$logger->info($resp->getHTTPStatus() . ': ' . $resp->getRawBody());
-}	
+  // Postback Event
+  if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
+    $logger->info('Postback message has come');
+    continue;
+  }
+  // Location Event
+  if  ($event instanceof LINE\LINEBot\Event\MessageEvent\LocationMessage) {
+    $logger->info("location -> ".$event->getLatitude().",".$event->getLongitude());
+    $outputText = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Why sent me your location. Huh!?", $event->getLatitude(), $event->getLongitude());
+    continue;
+  }
+  
+  // Message Event = TextMessage
+  
+  if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+		$messageText=strtolower(trim($event->getText()));
+		switch ($messageText) {
+		case "text" : 
+			$messageText=strtolower(trim($event->getText()));
+			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("Na na na na na na Batman !!!!");
+			break;
+		case "location" :
+			$outputText = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder("Eiffel Tower", "Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France", 48.858328, 2.294750);
+			break;
+		case "image" :
+			$img_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpF37MicznKpvrBvb0syRnKTnb1iEmhUOiEiSQHqHoUCayICQ9frR9Xg";
+			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
+			break;	
+		default :
+			$outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("demo command: text, location, button, confirm to test message template");	
+			break;
+		}
 
-
+		$response = $bot->replyMessage($event->getReplyToken(), $outputText);
+	}
+}  
