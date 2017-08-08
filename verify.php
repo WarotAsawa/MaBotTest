7f26ec06ecc500a1233052a1bc3ec828e9d6167f
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-08 22:13:32 +0700

    Add Verify

---
+++ verify.php
@@ -0,0 +1,18 @@
+<?php
+$access_token = 'tYLkTfUwWhH5WVunO5G0QhjUYgiAH4Bd0Bb+oFw0pfis0E0cibf6U73f7gdZid4cUcAURkLMr3D3qW3CfRbuFw3XubbKtHHY14ncqIhRpOwaB5c0BFol/ca78jdM5uCj+bDPDMfEA8bOT/cC0AAV8gdB04t89/1O/w1cDnyilFU='
+$channel_id = '1529179895'
+$channel_secret = '29834a3e45690862a8876a576bac2172'
+$userid = 'Ua9222d61b45ff88cc7315440f4f285f3'
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
