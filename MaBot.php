74bae7affa36a7856b8495f4d73511f4ae2daf04
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-12 20:58:09 +0700

    Add question

--- MaBot.php
+++ MaBot.php
@@ -210,7 +210,7 @@ function replyGreets($tempBot, $event, $logger) {
 function replyQuestion($tempBot, $event, $logger) {
 	$messageText=strtolower(trim($event->getText()));
 	$secondPersonName = getRandomText(' my fellow machine.', ' my master.', ' fellas.', ' ma friend.',' my fellow mutant.');
-	if (isStartWithText($messageText,'What') 
+	if (isStartWithText($messageText,'What') {
 		if (isContain($messageText,'your','name') {
 			$answerText = getRandomText(
 				'Don\'you see my name above?',
@@ -247,6 +247,7 @@ function replyQuestion($tempBot, $event, $logger) {
 			return true;
 		}
 	}
+}
 function replyJokes($tempBot, $event, $logger) {
 	$messageText=strtolower(trim($event->getText()));
 	if (isContain($messageText,'tell','me','joke')) {
