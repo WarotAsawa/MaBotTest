782a30c8fa11129e46a605d6df3ec58208dec3a6
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-25 08:52:24 +0700

    db

--- MaBot.php
+++ MaBot.php
@@ -1,11 +1,8 @@
-<?php 
-include("AllResponse.php");
-include("AllCriteria.php");
-?>;
 <?php
 
 require_once './vendor/autoload.php';
-
+require_once './AllResponse.php';
+require_once './AllCriteria.php';
 //Include library
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\Constant\HTTPHeader;
