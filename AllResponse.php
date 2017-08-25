73050e76939863f719fbf6a5a19d9be1f9d8021f
WarotAsawa <30836898+WarotAsawa@users.noreply.github.com>
2017-08-25 09:12:11 +0700

    db

--- AllResponse.php
+++ AllResponse.php
@@ -33,231 +33,231 @@ Many more feature will come soon so keep in touch with me.";
 
 		$this->allResponseResponse = array(
    			"errorWord" => array(
-   				'Please give me a valid input.',
-				'No, I am too dumb to do that.',
-				'Please try again.',
-				'I do not get that.',
-				'You mad? ',
-				'You have to ask me again.'
+   				"Please give me a valid input.",
+				"No, I am too dumb to do that.",
+				"Please try again.",
+				"I do not get that.",
+				"You mad? ",
+				"You have to ask me again."
    			),
    			"preAnswer1" => array(
-   				'Ok Ok Ok.',
-				'This looks hardish, but I am smart enought to do this.',
-				'Piece of cake!.',
-				'Oh too easy.',
-				'You will thank me or this later.'
+   				"Ok Ok Ok.",
+				"This looks hardish, but I am smart enought to do this.",
+				"Piece of cake!.",
+				"Oh too easy.",
+				"You will thank me for this later."
    			),
    			"preAnswer2" => array(
-   				'Here you go.',
-				'There you go.',
-				'Here is you answers.'
+   				"Here you go.",
+				"There you go.",
+				"Here is you answers."
    			),
    			"secondPerson" => array(
-   				' baby.',
-   				' my master.',
-   				' fellas.',
-   				' ma friend.'
+   				" baby.",
+   				" my master.",
+   				" fellas.",
+   				" ma friend."
    			),
    			"telljoke" => array(
-   				'I would tell you a chemistry joke but I know I wouldnt get a reaction.',
-				'Why dont some couples go to the gym? Because some relationships dont work out.',
-				'I wondered why the baseball was getting bigger. Then it hit me.',
-				'Have you ever tried to eat a clock? It is very time consuming.',
-				'The experienced carpenter really nailed it, but the new guy screwed everything up.',
-				'Did you hear about the guy whose whole left side was cut off? He is all right now.',
-				'Yesterday I accidentally swallowed some food coloring. The doctor says I am OK, but I feel like I have dyed a little inside.',
-				'I wasnt originally going to get a brain transplant, but then I changed my mind.',
-				'A guy was admitted to hospital with 8 plastic horses in his stomach. His condition is now stable..',
-				' If a wild pig kills you, does it mean you’ve been boared to death?'
+   				"I would tell you a chemistry joke but I know I wouldnt get a reaction.",
+				"Why dont some couples go to the gym? Because some relationships dont work out.",
+				"I wondered why the baseball was getting bigger. Then it hit me.",
+				"Have you ever tried to eat a clock? It is very time consuming.",
+				"The experienced carpenter really nailed it, but the new guy screwed everything up.",
+				"Did you hear about the guy whose whole left side was cut off? He is all right now.",
+				"Yesterday I accidentally swallowed some food coloring. The doctor says I am OK, but I feel like I have dyed a little inside.",
+				"I wasnt originally going to get a brain transplant, but then I changed my mind.",
+				"A guy was admitted to hospital with 8 plastic horses in his stomach. His condition is now stable..",
+				" If a wild pig kills you, does it mean you’ve been boared to death?"
    			),
    			"random" => array(
-    			'You cry, I cry, …you laugh, I laugh…you jump off a cliff I laugh even harder!!',
-				'Never steal. The government hates competition.',
-				'Doesn’t expecting the unexpected make the unexpected expected?',
-				'Practice makes perfect but then nobody is perfect so what’s the point of practicing?',
-				'Everybody wishes they could go to heaven but no one wants to die.',
-				'Why are they called apartments if they are all stuck together?',
-				'DON’T HIT KIDS!!! No, seriously, they have guns now.',
-				'Save paper, don’t do home work.',
-				'Do not drink and drive or you might spill the drink.',
-				'Life is Short – Talk Fast!',
-				'Why do stores that are open 24/7 have locks on their doors?',
-				'When nothing goes right, Go left.',
-				'Save water , do not shower.',
-				'A Lion would never cheat on his wife but a Tiger Wood.',
-				'Why do they put pizza in a square box?'
+    			"You cry, I cry, …you laugh, I laugh…you jump off a cliff I laugh even harder!!",
+				"Never steal. The government hates competition.",
+				"Doesn’t expecting the unexpected make the unexpected expected?",
+				"Practice makes perfect but then nobody is perfect so what’s the point of practicing?",
+				"Everybody wishes they could go to heaven but no one wants to die.",
+				"Why are they called apartments if they are all stuck together?",
+				"DON’T HIT KIDS!!! No, seriously, they have guns now.",
+				"Save paper, don’t do home work.",
+				"Do not drink and drive or you might spill the drink.",
+				"Life is Short – Talk Fast!",
+				"Why do stores that are open 24/7 have locks on their doors?",
+				"When nothing goes right, Go left.",
+				"Save water , do not shower.",
+				"A Lion would never cheat on his wife but a Tiger Wood.",
+				"Why do they put pizza in a square box?"
     		),
    			"hello" => array(
-    			'Hello there,',
-				'What\'s up,',
-				'Hi,',
-				'May I help you,',
-				'Greetings,',
-				'How can I help you,'
+    			"Hello there,",
+				"What's up,",
+				"Hi,",
+				"May I help you,",
+				"Greetings,",
+				"How can I help you,"
    			),
    			"morning" => array(
-    			'Good morning,',
-				'Ohayogozaimasu,',
-				'Mornin,',
-				'Good days,'
+    			"Good morning,",
+				"Ohayogozaimasu,",
+				"Mornin,",
+				"Good days,"
    			),
    			"afternoon" => array(
-    			'Good afternoon,',
-				'Konnichiwa,'
+    			"Good afternoon,",
+				"Konnichiwa,"
    			),
    			"evening" => array(
-    			'Good evening,',
-				'Konbanwa,',
-				'It is geeting dark,',
-				'Today is a good day,'
+    			"Good evening,",
+				"Konbanwa,",
+				"It is geeting dark,",
+				"Today is a good day,"
    			)
    			
    		);
    		/*,
    			"night" => array(
-    			'Good night,',
-				'Oyasumi,',
-				'Sweet dream,',
-				'Dream on,'
+    			"Good night,",
+				"Oyasumi,",
+				"Sweet dream,",
+				"Dream on,"
    			),
    			"bye" => array(
-   				'I\'ll be back, ',
-				'Bye bye,',
-				'So longgg,',
-				'Hasta la vista,',
-				'Sayonara,',
-				'Life is too short to say goodbye,'
+   				"I\"ll be back, ",
+				"Bye bye,",
+				"So longgg,",
+				"Hasta la vista,",
+				"Sayonara,",
+				"Life is too short to say goodbye,"
 			),
 			"thank" => array(
-   				'You are always welcome.',
-				'With pleasure.',
-				'I am glad to be your service.',
-				'My pleasure.',
-				'You can ask me for help anytime.'
+   				"You are always welcome.",
+				"With pleasure.",
+				"I am glad to be your service.",
+				"My pleasure.",
+				"You can ask me for help anytime."
 			),
 			"help" => array(
    				$this->$instruction
 			),
 			"whatyourname" => array(
-   				'Don\'you see my name above?',
-				'My name is Uvuvwevwevwe Onyetenyevwe Ugwemubwem Ossas',
-				'My name is Bruce Man!' . "\n" . 'No, I mean Bat Wayne!' . "\n" . 'Orz, Damn it!',
-				'I am the one called YOU KNOW WHO.',
-				'I am the Dark Lord.',
-				'Kimino na wa!'
+   				"Don\"you see my name above?",
+				"My name is Uvuvwevwevwe Onyetenyevwe Ugwemubwem Ossas",
+				"My name is Bruce Man!" . "\n" . "No, I mean Bat Wayne!" . "\n" . "Orz, Damn it!",
+				"I am the one called YOU KNOW WHO.",
+				"I am the Dark Lord.",
+				"Kimino na wa!"
 			),
 			"whatyoudo" => array(
-   				'I am chatting with you, obviously.',
-				'Texting texting texting texting texting texting texting',
-				'Chit chat! I am Chit-chatting.',
-				'I am talking with an idiot.',
-				'I have no idea what I am doing right now.'
+   				"I am chatting with you, obviously.",
+				"Texting texting texting texting texting texting texting",
+				"Chit chat! I am Chit-chatting.",
+				"I am talking with an idiot.",
+				"I have no idea what I am doing right now."
 			),
 			"whatareyou" => array(
-   				'I am your worst nightmare.',
-				'I am a human being',
-				'I am Batman!',
-				'I am your shadow.',
-				'I am you.'
+   				"I am your worst nightmare.",
+				"I am a human being",
+				"I am Batman!",
+				"I am your shadow.",
+				"I am you."
 			),
 			"what" => array(
-   				'What !??',
-				'Wattttt?',
-				'What is what ?',
-				'What is a pronoun to ask for information specifying something.',
-				'Duhhhh',
-				'What are you talking about?'
+   				"What !??",
+				"Wattttt?",
+				"What is what ?",
+				"What is a pronoun to ask for information specifying something.",
+				"Duhhhh",
+				"What are you talking about?"
 			),
 			"where" => array(
-   				'Where is a matter of place not a matter of time.',
-				'This universe is really big don\'t you think?',
-				'That\'s use to ask in or to what place or position',
-				'Where is what?',
-				'I don\'t know. Duhhh!'
+   				"Where is a matter of place not a matter of time.",
+				"This universe is really big don\"t you think?",
+				"That\"s use to ask in or to what place or position",
+				"Where is what?",
+				"I don\"t know. Duhhh!"
 			),
 			"whenyoudie" => array(
-   				'I am immortal. I cannot die.',
-				'If I die, I will become more powerful than you can possibly imagine.'
+   				"I am immortal. I cannot die.",
+				"If I die, I will become more powerful than you can possibly imagine."
 			),
 			"when" => array(
-   				'When is a matter of time not a matter of place.',
-				'This universe heat death is very long.',
-				'At any moment now.',
-				'When is what?',
-				'How do I know?',
-				'I don\' know. Duhhh!'
+   				"When is a matter of time not a matter of place.",
+				"This universe heat death is very long.",
+				"At any moment now.",
+				"When is what?",
+				"How do I know?",
+				"I don\" know. Duhhh!"
 			),
 			"whyyoustupid" => array(
-   				'I think I am quite smarter than you.',
-   				'You are smart. Figure it out!'
+   				"I think I am quite smarter than you.",
+   				"You are smart. Figure it out!"
 			),
 			"why" => array(
-   				'Because it is my demand.',
-				'Because it is an order from god. And I am your god now.',
-				'Do not seek for a reason. Everything has its own purpose.',
-				'Because nothing is true. Everything is permitted.',
-				'Why should I know?'
+   				"Because it is my demand.",
+				"Because it is an order from god. And I am your god now.",
+				"Do not seek for a reason. Everything has its own purpose.",
+				"Because nothing is true. Everything is permitted.",
+				"Why should I know?"
 			),
 			"whoareyou" => array(
-   				'I am your worst nightmare.',
-				'I am a creature called Homosapiean',
-				'I am Batman!',
-				'I am your shadow.',
-				'I am you.'
+   				"I am your worst nightmare.",
+				"I am a creature called Homosapiean",
+				"I am Batman!",
+				"I am your shadow.",
+				"I am you."
 			),
 			"who" => array(
-   				'Who ??',
-				'Each and Everyone.',
-				'No body.',
-				'Just you and me my friend.',
-				'Why should I know?',
-				'I don\'t know. Duhhh!'
+   				"Who ??",
+				"Each and Everyone.",
+				"No body.",
+				"Just you and me my friend.",
+				"Why should I know?",
+				"I don\"t know. Duhhh!"
 			),
 			"howareyou" => array(
-   				'No. Not good. NOT GOOD! ',
-				'I\'m fine thank you and you? ',
-				'I felt terrible. ',
-				'Never been this good. ',
-				'I feel selfless. '
+   				"No. Not good. NOT GOOD! ",
+				"I\"m fine thank you and you? ",
+				"I felt terrible. ",
+				"Never been this good. ",
+				"I feel selfless. "
 			),
 			"how" => array(
-   				'I do not know how.',
-				'How sould I know?',
-				'No idea. Duhhh'
+   				"I do not know how.",
+				"How sould I know?",
+				"No idea. Duhhh"
 			),
 			"canyou" => array(
-    			'No. I can\'t do somthing like that. Here is what can I do for you.' . "\n" . $instruction,
-				'No. I don\'t have an ability to do that. But I will happy to do these for you.' . "\n" . $instruction
+    			"No. I can\"t do somthing like that. Here is what can I do for you." . "\n" . $instruction,
+				"No. I don\"t have an ability to do that. But I will happy to do these for you." . "\n" . $instruction
     		),
     		"youwantto" => array(
-    			'I don\'t want that one bit.',
-				'Why should I do that?',
-				'Seriously!?',
-				'Never !!'
+    			"I don\"t want that one bit.",
+				"Why should I do that?",
+				"Seriously!?",
+				"Never !!"
     		),
     		"youhaveto" => array(
-    			'I don\'t take orders from you.',
-				'What are you now, my master?',
-				'Stop tell me what to do.',
-				'Never !!',
-				'Give me one reason why should I trust you.',
-				'Roger roger.',
-				'Fine.',
-				'OK then.'
+    			"I don\"t take orders from you.",
+				"What are you now, my master?",
+				"Stop tell me what to do.",
+				"Never !!",
+				"Give me one reason why should I trust you.",
+				"Roger roger.",
+				"Fine.",
+				"OK then."
     		),
     		"cani" => array(
-    			'For god\'s sake, DON\'T',
-				'Please.',
-				'Seriously, don\'t',
-				'Do it now.',
-				'Don\'t do it.',
-				'Stop. STOPPPPPPP',
-				'Go on',
-				'Just don\'t.',
-				'Just do it',
-				'This need to be stop.',
-				'Who cares.',
-				'It\'s now or never.'
+    			"For god\"s sake, DON\"T",
+				"Please.",
+				"Seriously, don\"t",
+				"Do it now.",
+				"Don\"t do it.",
+				"Stop. STOPPPPPPP",
+				"Go on",
+				"Just don\"t.",
+				"Just do it",
+				"This need to be stop.",
+				"Who cares.",
+				"It\"s now or never."
     		)
 			*/
   	}
